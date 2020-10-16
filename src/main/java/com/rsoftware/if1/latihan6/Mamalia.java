53fa5acdbcc8ddea90200e01be0ca47db588bab9
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 15:36:48 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan6/Mamalia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan6;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Deskripsi Program : Kambing Static & Konstanta
+ * 
+ */
+
+public class Mamalia {
+    // Variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+}
